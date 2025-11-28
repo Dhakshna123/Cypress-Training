ea547b2a7ccc57e3e50e3cd8aff5cd3b4c555a1a
Dhakshna123 <Dhakshanamurthy.M@trane.com>
2025-11-28 15:39:57 +0530

    Ensemble

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-     projectId: "2o5oeo",
+     projectId: "e8tkgp",
   },
 });
